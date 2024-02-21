ab25f3f25e1d1d7f95d475fc6ccf735ecaf74ac5
Deiver Vazquez <dvazquez@lsv-tech.com>
2024-02-21 15:47:59 -0500

    remove BuildContext

--- lib/utils/injector.dart
+++ lib/utils/injector.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:shoes_shop_app/modules/home/controllers/controllers.dart';
@@ -7,8 +6,8 @@ import 'package:shoes_shop_app/modules/home/data/use_cases/uc_home.dart';
 
 class Injector {
   static List<SingleChildWidget> dependencies = [
-    BlocProvider(create: (BuildContext context) => HomeBloc()),
-    BlocProvider(create: (BuildContext context) => CategoryBloc()),
+    BlocProvider(create: (_) => HomeBloc()),
+    BlocProvider(create: (_) => CategoryBloc()),
     BlocProvider(
       create: (_) => ProductBloc(
         getProductsUseCase: GetProductsUseCase(
